e979eb3666486f9a36ddc0f6985d1115bcdbdb5b
jaavant <jaavant@wpi.edu>
2017-09-11 16:42:09 -0400

    finished another algorithms

--- src/edu/wpi/Project1/methods/BeamSearch.java
+++ src/edu/wpi/Project1/methods/BeamSearch.java
@@ -2,9 +2,11 @@ package edu.wpi.Project1.methods;
 
 import edu.wpi.Project1.util.Graph;
 import edu.wpi.Project1.util.NewNode;
+import sun.awt.image.ImageWatched;
 
 import java.util.ArrayDeque;
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -20,14 +22,17 @@ public class BeamSearch extends Algorithm {
 
     @Override
     public ArrayDeque<NewNode> add(ArrayDeque<NewNode> frontier, List<NewNode> children, Graph graph) {
-        children.sort(new Comparator<NewNode>() {
+        LinkedList<NewNode> sortedList = new LinkedList(children);
+        sortedList.addAll(frontier);
+
+        sortedList.sort(new Comparator<NewNode>() {
             @Override
             public int compare(NewNode o1, NewNode o2) {
-                if (o1.getH() < o2.getH()){
-                    return -1;
-                }
-                else if(o1.getH() > o2.getH()){
+                if(o2.getPath().size() < o1.getPath().size()){
                     return 1;
+                }
+                else if((o2.getPath().size() > o1.getPath().size())){
+                    return -1;
                 }
                 else{
                     if(o2.getLetter() > o1.getLetter()){
@@ -42,10 +47,72 @@ public class BeamSearch extends Algorithm {
                 }
             }
         });
-        children.subList(0,this.k);
-        for(int i = children.size(); i >= 0; i--){
-            frontier.addFirst(children.get(i));
+
+        boolean beamNodes;
+
+        if(sortedList.size() > 0){
+            beamNodes = true;
+            int pathSize = sortedList.getFirst().getPath().size();
+            for(NewNode node : sortedList){
+                if(node.getPath().size() != pathSize){
+                    beamNodes = false;
+                    break;
+                }
+            }
         }
+        else{
+            beamNodes = false;
+        }
+
+        frontier.clear();
+
+        if(beamNodes) {
+            int max = (sortedList.size() < this.k) ? sortedList.size() : this.k;
+
+            sortedList.sort(new Comparator<NewNode>() {
+                @Override
+                public int compare(NewNode o1, NewNode o2) {
+                    if(o2.getH() > o1.getH()){
+                        return -1;
+                    }
+                    else if(o2.getH() < o1.getH()){
+                        return 1;
+                    }
+                    else{
+                        if(o2.getLetter() > o1.getLetter()){
+                            return -1;
+                        }
+                        else if (o1.getLetter() > o2.getLetter()){
+                            return 1;
+                        }
+                        else{
+                            return 0;
+                        }
+                    }
+                }
+            });
+            List<NewNode> newList = (List<NewNode>) sortedList.subList(0, max);
+            newList.sort(new Comparator<NewNode>() {
+                             @Override
+                             public int compare(NewNode o1, NewNode o2) {
+                                 if(o2.getLetter() > o1.getLetter()){
+                                     return -1;
+                                 }
+                                 else if (o1.getLetter() > o2.getLetter()){
+                                     return 1;
+                                 }
+                                 else{
+                                     return 0;
+                                 }
+                             }
+                         });
+            frontier.addAll(newList);
+        }
+        else{
+            frontier.addAll(sortedList);
+        }
+
+
         return frontier;
     }
 }
