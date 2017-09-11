222fca100d69a5fab4c7aff3ddf318d7e862cc47
jaavant <jaavant@wpi.edu>
2017-09-11 08:01:02 -0400

    finished another algorithms

---
+++ src/edu/wpi/Project1/methods/HillClimbing.java
@@ -0,0 +1,17 @@
+package edu.wpi.Project1.methods;
+
+import edu.wpi.Project1.util.Graph;
+import edu.wpi.Project1.util.NewNode;
+
+import java.util.ArrayDeque;
+import java.util.List;
+
+/**
+ * Created by John on 9/11/2017.
+ */
+public class HillClimbing extends Algorithm {
+    @Override
+    public ArrayDeque<NewNode> add(ArrayDeque<NewNode> frontier, List<NewNode> children, Graph graph) {
+        return null;
+    }
+}
